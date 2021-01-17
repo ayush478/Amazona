8e26dc774795a3110e094eb25b64de986e25dfca
Ayush <ayushprajapati47@gmail.com>
2021-01-17 19:11:37 +0530

    Rating

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+import {Navbar,Nav,Container} from 'react-bootstrap'
 
 
 const Header = () => {
@@ -7,15 +9,26 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">Amazona</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                <Nav.Link href="/cart"><i className="fas fa-shopping-cart mr-1"></i>Cart</Nav.Link>
-                <Nav.Link href="/login"><i className="fas fa-user mr-1"></i>Sign In</Nav.Link>
-                
-    </Nav>
-     </Navbar.Collapse>
+                        <LinkContainer to='/'>
+                            <Navbar.Brand >Amazona</Navbar.Brand>
+                        </LinkContainer>
+                    
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <LinkContainer to='/cart'>
+                            <Nav.Link ><i className="fas fa-shopping-cart mr-1"></i>
+                            Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'> 
+                            <Nav.Link ><i className="fas fa-user mr-1"></i>
+                            Sign In
+                            </Nav.Link>
+                        </LinkContainer>
+                        
+                        </Nav>
+                        </Navbar.Collapse>
                 </Container>
  
     
